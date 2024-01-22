99f8427ba16856293d1f4b0a62285f115b542586
Twisttooth <49693739+Twisttooth@users.noreply.github.com>
2024-01-22 19:02:51 +0200

    define Thing superclass, for all Things

--- sorter.rb
+++ sorter.rb
@@ -1,158 +1,157 @@
 # define Thing superclass, for all Things
 class Thing
-  attr_accessor( :name, :description )
+  attr_reader :name, :description
   @@num_things = 0
-  
-  def initialize( aName, aDescription )
+
+  def initialize(aName, aDescription)
     @name = aName.capitalize
     @description = aDescription
     @@num_things += 1
-    puts( "Thing.initialize: #{self.inspect}\n\n" )
   end
 
   def things
-    return @@num_things
+    @@num_things
   end
-
-  def things=(num)
-    @things = num
-  end
-
 end
 
 # define Place class for coops and pastures
 class Place < Thing
-
-  def initialize( aName, aDescription, sizeIs, predatorProof )
-    super( aName, aDescription )
+  def initialize(aName, aDescription, sizeIs, predatorProof)
+    super(aName, aDescription)
     @size = sizeIs
     @predator_proof = predatorProof
-    puts( "init.Place: #{self.inspect}\n\n" )
   end
-
 end
 
 # define class for Birds
 class Bird < Thing
-  
-  def initialize( aName, aDescription, isSpecies )
-    super( aName, aDescription )
+  def initialize(aName, aDescription, isSpecies)
+    super(aName, aDescription)
     @species = isSpecies
-    puts( "init.Bird: #{self.inspect}\n\n" )
   end
-
 end
 
 # def menu options
-def status_menu()
+def status_menu
   luku = Thing.new("things", "amount of things")
 
-  puts( "Farm Status" )
-  puts( "Things around the farm: #{(luku.things - 1) / 2}\n\n" )
+  puts "Farm Status"
+  puts "Things around the farm: #{(luku.things - 1) / 2}\n\n"
 
-  puts( "Untitled goose sorter.\n\n" )
-  puts( "1. Status\n2. Manage birds\n3. Manage farm areas\n\n" )
+  puts "Untitled goose sorter.\n\n"
+  puts "1. Status\n2. Manage birds\n3. Manage farm areas\n\n"
 end
 
 # bird manage menu
-def bird_menu()
+def bird_menu
   ans = ''
-  puts( "Manage birds\n\n" )
-  puts( "1. Add Bird\n2. Remove Bird\n3. Move Bird\n\n" )
-  until ans == 'b' do  
-    puts( "[ Enter 1, 2, 3 or 'b'? ]" )
-    print( "> " )
-    ans = gets[0].chr().downcase()
-    
-    case ans 
-      when '1' then
-        ch = ''
-        puts( "Goose, duck, or Chicken?" )
-        puts( "1. Goose\n2. Duck\n3. Chicken" )
-        until ch == 'b' do
-          puts( "[ Enter 1, 2, 3 or 'b'? ]" )
-          print( "> " )
-          ch = gets[0].chr().downcase()
-          
-          case ch
-            when '1' then
-              puts( "Name your goose: " ) 
-              name = gets
-              puts( "Describe your goose: " )
-              description = gets
-              goose = Bird.new(name, description, "goose")
-              puts("Goose: #{goose.inspect}")
+  puts "Manage birds\n\n"
+  puts "1. Add Bird\n2. Remove Bird\n3. Move Bird\n\n"
+  until ans == :b do
+    puts "[ Enter 1, 2, 3 or 'b'? ]"
+    print "> "
+    ans = gets[0].chr.downcase.to_sym
 
-            when '2' then
-              puts( "Name your duck: " ) 
-              name = gets
-              puts( "Describe your duck: " )
-              description = gets
-              duck = Bird.new(name, description, "duck")
-              puts("Duck: #{duck.inspect}")
+    case ans
+    when :1
+      ch = ''
+      puts "Goose, duck, or Chicken?"
+      puts "1. Goose\n2. Duck\n3. Chicken"
+      until ch == :b do
+        puts "[ Enter 1, 2, 3 or 'b'? ]"
+        print "> "
+        ch = gets[0].chr.downcase.to_sym
 
-            when '3' then puts( "Chicken" )
-              puts( "Name your chicken: " ) 
-              name = gets
-              puts( "Describe your chicken: " )
-              description = gets
-              chicken = Bird.new(name, description, "chicken")
-              puts("Chicken: #{chicken.inspect}")
+        case ch
+        when :1
+          puts "Name your goose: "
+          name = gets.chomp
+          puts "Describe your goose: "
+          description = gets.chomp
+          goose = Bird.new(name, description, "goose")
+          puts "Goose: #{goose.inspect}"
 
-          end
-          puts( "Goose, duck, or Chicken?" )
-          puts( "1. Goose\n2. Duck\n3. Chicken" )
+        when :2
+          puts "Name your duck: "
+          name = gets.chomp
+          puts "Describe your duck: "
+          description = gets.chomp
+          duck = Bird.new(name, description, "duck")
+          puts "Duck: #{duck.inspect}"
+
+        when :3
+          puts "Name your chicken: "
+          name = gets.chomp
+          puts "Describe your chicken: "
+          description = gets.chomp
+          chicken = Bird.new(name, description, "chicken")
+          puts "Chicken: #{chicken.inspect}"
         end
-        puts( "Manage birds\n\n" )
-        puts( "1. Add Bird\n2. Remove Bird\n3. Move Bird\n\n" )
-      when '2' then puts( "remove" )
-      when '3' then puts( "move" )
-    end	  
+
+        puts "Goose, duck, or Chicken?"
+        puts "1. Goose\n2. Duck\n3. Chicken"
+      end
+      puts "Manage birds\n\n"
+      puts "1. Add Bird\n2. Remove Bird\n3. Move Bird\n\n"
+
+    when :2
+      puts "remove"
+
+    when :3
+      puts "move"
+    end
   end
-  
-  puts( "Untitled goose sorter.\n\n" )
-  puts( "1. Status\n2. Manage birds\n3. Manage farm areas\n\n" )
+
+  puts "Untitled goose sorter.\n\n"
+  puts "1. Status\n2. Manage birds\n3. Manage farm areas\n\n"
 end
 
 # farm manage menu
-def farm_menu()
+def farm_menu
   ans = ''
-  puts( "Manage farm\n\n" )
-  puts( "1. Add coop\n2. Remove coop\n3. Move coop\n\n" )
-  until ans == 'b' do  
-    puts( "[ Enter 1, 2, 3 or 'b'? ]" )
-    print( "> " )
-    ans = gets[0].chr().downcase()
-    
-    case ans 
-      when '1' then puts( "add" )
-        puts( "Name your coop: " ) 
-        name = gets
-        puts( "Describe your coop: " )
-        description = gets
-        coop = Place.new(name, description, 20, TRUE)
-        puts("New Coop: #{coop.inspect}")
+  puts "Manage farm\n\n"
+  puts "1. Add coop\n2. Remove coop\n3. Move coop\n\n"
+  until ans == :b do
+    puts "[ Enter 1, 2, 3 or 'b'? ]"
+    print "> "
+    ans = gets[0].chr.downcase.to_sym
 
-      when '2' then puts( "remove" )
-      when '3' then puts( "move" )
-    end	  
-  
+    case ans
+    when :1
+      puts "add"
+      puts "Name your coop: "
+      name = gets.chomp
+      puts "Describe your coop: "
+      description = gets.chomp
+      coop = Place.new(name, description, 20, true)
+      puts "New Coop: #{coop.inspect}"
+
+    when :2
+      puts "remove"
+
+    when :3
+      puts "move"
+    end
   end
-  puts( "Untitled goose sorter.\n\n" )
-  puts( "1. Status\n2. Manage birds\n3. Manage farm areas\n\n" )
+  puts "Untitled goose sorter.\n\n"
+  puts "1. Status\n2. Manage birds\n3. Manage farm areas\n\n"
 end
 
 # 'menu' loop
 ans = ''
-puts( "Untitled goose sorter.\n\n" )
-puts( "1. Status\n2. Manage birds\n3. Manage farm areas\n\n" )
-until ans == 'q' do  
-  puts( "[ Enter 1, 2, 3 or 'q'? ]" )
-  print( "> " )
-  ans = gets[0].chr().downcase()
-  case ans 
-	when '1' then status_menu()
-	when '2' then bird_menu()
-	when '3' then farm_menu()
-  end	  
-end 
+puts "Untitled goose sorter.\n\n"
+puts "1. Status\n2. Manage birds\n3. Manage farm areas\n\n"
+until ans == :q do
+  puts "[ Enter 1, 2, 3 or 'q'? ]"
+  print "> "
+  ans = gets[0].chr.downcase.to_sym
+
+  case ans
+  when :1
+    status_menu()
+  when :2
+    bird_menu()
+  when :3
+    farm_menu()
+  end
+end
